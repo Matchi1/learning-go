35d93066ddd45a28b0aadeebc31c8ab9dac0f08e
Chan Mathieu <pro.matchi1@gmail.com>
2022-05-17 21:32:18 +0200

    reflect and append

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+    "testing"
+    "reflect"
+)
 
 func TestSum(t *testing.T) {
     t.Run("Collection of 5 numbers", func(t *testing.T) {
@@ -13,4 +16,14 @@ func TestSum(t *testing.T) {
             t.Errorf("Expected %d but got %d given, %v", want, got, numbers)
         }
     })
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
