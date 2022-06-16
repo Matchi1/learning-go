2c66ec88657130c46bf731529587a94040500477
Chan Mathieu <pro.matchi1@gmail.com>
2022-06-16 21:55:22 +0200

    06_wallet: v2

--- 06_pointors/wallet_test.go
+++ 06_pointors/wallet_test.go
@@ -5,14 +5,21 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-    wallet := Wallet{}
-
-    wallet.Deposit(Bitcoin(10))
-
-    got := wallet.Balance()
-    want := Bitcoin(10)
-
-    if want != got {
-        t.Errorf("got %s want %s", got, want)
+    assertBalance := func (t testing.TB, wallet Wallet, bitcoin Bitcoin) {
+        got := wallet.Balance()
+        want := bitcoin
+        if want != got {
+            t.Errorf("got %s want %s", got, bitcoin)
+        }
     }
+    t.Run("depositing", func (t *testing.T) {
+        wallet := Wallet{}
+        wallet.Deposit(Bitcoin(10))
+        assertBalance(t, wallet, Bitcoin(10))
+    })
+    t.Run("withdrawing", func (t *testing.T) {
+        wallet := Wallet{balance: Bitcoin(10)}
+        wallet.Withdraw(Bitcoin(10))
+        assertBalance(t, wallet, Bitcoin(0))
+    })
 }
