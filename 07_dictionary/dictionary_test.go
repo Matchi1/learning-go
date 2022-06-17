de8a27ed9d7021188db1311e5ccdd769873aff77
Mathieu Chan <mathieu.chan@qarnot-computing.com>
2022-06-17 10:34:50 +0200

    07_dictionary: v2

--- 07_dictionary/dictionary_test.go
+++ 07_dictionary/dictionary_test.go
@@ -5,10 +5,25 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+    t.Run("word is known", func (t *testing.T) {
+        got, _ := dictionary.Search("test")
+        want := "this is just a test"
 
-    assertStrings(t, got, want)
+        assertStrings(t, got, want)
+    })
+    t.Run("word is not known", func (t *testing.T) {
+        _, err := dictionary.Search("unknown")
+        want := ErrNotFound
+
+        assertErrors(t, err, want)
+    })
+}
+
+func assertErrors(t testing.TB, got, want error) {
+    t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q given", got, want)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
