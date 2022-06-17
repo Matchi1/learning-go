ab1ba8ffbfa25e8cf49fdc83f16f4562b0bea514
Mathieu Chan <mathieu.chan@qarnot-computing.com>
2022-06-17 15:25:46 +0200

    09_countdown: v4

--- 09_countdown/countdown.go
+++ 09_countdown/countdown.go
@@ -9,23 +9,30 @@ import (
 
 const finalWord = "Go!"
 const countDownStart = 3
+const write = "write"
+const sleep = "sleep"
 
 type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
-}
-
 type DefaultSleeper struct {}
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+type SpyCountdownOperations struct {
+	Calls []string
 }
 
 func (s *DefaultSleeper) Sleep() {
     time.Sleep(1 * time.Second)
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
--- 09_countdown/countdown_test.go
+++ 09_countdown/countdown_test.go
@@ -3,21 +3,41 @@ package main
 import (
     "testing"
     "bytes"
+    "reflect"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-    spySleeper := &SpySleeper{}
+    t.Run("Number of calls", func (t *testing.T) {
+        buffer := &bytes.Buffer{}
+        spy := &SpyCountdownOperations{}
 
-	Countdown(buffer, spySleeper)
+        Countdown(buffer, spy)
 
-	got := buffer.String()
-	want := "3\n2\n1\nGo!"
+        got := buffer.String()
+        want := "3\n2\n1\nGo!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-    if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
-    }
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    })
+    t.Run("Number of calls", func (t *testing.T) {
+        spy := &SpyCountdownOperations{}
+
+        Countdown(spy, spy)
+
+        want := []string{
+            "write",
+            "sleep",
+            "write",
+            "sleep",
+            "write",
+            "sleep",
+            "write",
+        }
+
+        if !reflect.DeepEqual(want, spy.Calls) {
+		    t.Errorf("wanted calls %v got %v", want, spy.Calls)
+        }
+
+    })
 }
