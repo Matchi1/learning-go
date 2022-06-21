775de77b61d16cff310be5aca9928eeb25bf3757
Mathieu Chan <mathieu.chan@qarnot-computing.com>
2022-06-21 11:50:43 +0200

    15_roman: v6

--- 15_roman/roman.go
+++ 15_roman/roman.go
@@ -9,7 +9,9 @@ type RomanNumeral struct {
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumeral{
+type RomanNumerals []RomanNumeral
+
+var allRomanNumerals = RomanNumerals {
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -37,4 +39,46 @@ func ConvertToRoman(arabic int) string {
 	}
 
 	return result.String()
+}
+
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
+func ConvertToArabic(roman string) int {
+	total := 0
+
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if couldBeSubtractive(i, symbol, roman) {
+			nextSymbol := roman[i+1]
+
+			// build the two character string
+			potentialNumber := string([]byte{symbol, nextSymbol})
+
+			// get the value of the two character string
+			value := allRomanNumerals.ValueOf(potentialNumber)
+
+			if value != 0 {
+				total += value
+				i++ // move past this character too for the next loop
+			} else {
+				total++
+			}
+		} else {
+			total++
+		}
+	}
+	return total
+}
+
+func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
+	return index+1 < len(roman) && currentSymbol == 'I'
 }
